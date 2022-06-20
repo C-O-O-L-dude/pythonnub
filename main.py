82632178b5296e1f9aa22814a444a35885e1e42b
C-O-O-L-dude <87819119+C-O-O-L-dude@users.noreply.github.com>
2022-06-20 15:24:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import telebot
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
-@bot.add_event_handler(
-          start_handler, events.NewMessage(pattern=command_process(get_command("START")))
-     )
+@bot.message_handler(Commands=['start']
+                     
+  def start(message):
+     bot.send_message(mesaage.chat.id,"Hi there 👋, You can easily get shinchan movies in hindi.🍀 just send /getlink")
+                   
+@bot.message_handler(Commands=['getlink']     
+                     
+  def getlink(mesaage):
+     bot.send_message(message.chat.id,"from here you can download shinchan movies...."
